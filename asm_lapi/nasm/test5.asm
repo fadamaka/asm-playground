ab0fd7b8c1fdff0efbe5e0d5576b082ec426970f
fadamaka <hcstnh@gmail.com>
2025-02-07 01:47:02 +0100

    another proper helloworld

---
+++ asm_lapi/nasm/test5.asm
@@ -0,0 +1,23 @@
+section .data
+    hello db 'Hello, World!', 0  
+    helloLen equ $ - hello        
+
+section .text
+    global main
+    extern ExitProcess, WriteConsoleA, GetStdHandle
+
+main:
+    sub rsp, 28h               
+    mov rcx, -11                
+    call GetStdHandle
+
+    ; Escribir el mensaje en la consola
+    mov r9, 0                
+    mov r8, helloLen          
+    mov rdx, hello            
+    mov rcx, rax               
+    call WriteConsoleA        
+
+    ; Terminar el programa
+    mov rcx, 0                 
+    call ExitProcess    
